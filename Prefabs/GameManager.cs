f95e0896964f19383a676f4efa47f24210d4b380
Iniesta-sys <76072912+Iniesta-sys@users.noreply.github.com>
2023-06-16 18:28:13 +0200

    Add files via upload

---
+++ Prefabs/GameManager.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+
+    bool gameHasEnded = false;
+
+    public float restartDelay = 3f;
+
+    public GameObject completeLevelUI;
+
+    public void CompleteLevel()
+
+    {
+        completeLevelUI.SetActive(true);
+    }
+    public void EndGame()
+
+
+   
+    {
+        if (gameHasEnded == false)
+        {
+            gameHasEnded = true;
+            Debug.Log("Game Over");
+            Invoke("Restart", restartDelay);
+        }
+    }
+
+
+    void Restart()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
